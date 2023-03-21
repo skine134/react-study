83c03417b806d79e9e1253cddbecbb4f3178f99c
오세환 <osehwan@osehwan-ui-MacBookAir.local>
2023-03-22 06:22:45 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from "react"
 import { useState, useEffect } from 'react'
-import MovieList from "./pages/MovieList";
-import CoinTracker from "./pages/CoinTracker";
-import TodoManager from "./pages/TodoManager";
+import MovieList from "./routes/MovieList";
+import CoinTracker from "./routes/CoinTracker";
+import TodoManager from "./routes/TodoManager";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
-import Detail from "./pages/Detail";
+import Detail from "./routes/Detail";
 function App() {
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path="/to-do-manager">
-          <TodoManager />
-        </Route>
-        <Route path="/coin-tracker">
-          <CoinTracker />
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <MovieList />
-        </Route>
-      </Switch>
-    </Router>
+    <h1>hello</h1>
   );
 }
 
