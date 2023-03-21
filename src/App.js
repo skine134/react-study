8e5353bfe4e35b1ec892a793c5ad70bcf777bfb5
오세환 <osehwan@osehwan-ui-MacBookAir.local>
2023-03-22 05:54:47 +0900

    :bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Detail from "./pages/Detail";
 function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/to-do-manager">
           <TodoManager />
